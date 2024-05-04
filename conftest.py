605bb910b45d8f3b765c707474f50094f2ce6880
Sasaraf1992 <zelen20@gmail.com>
2024-05-04 23:40:49 +0300

    На отправку 2

--- conftest.py
+++ conftest.py
@@ -2,6 +2,9 @@ import allure
 import pytest
 from endpoints.create_courier_object import CreateCourier
 from data import ScooterTestData as SD
+from faker import Faker
+
+fake = Faker()
 
 
 @allure.step("Создание фейкового курьера")
@@ -18,3 +21,12 @@ def fake_courier_required_fields():
     co = CreateCourier()
     co.courier_creation(data=SD.FAKE_COURIER_CREATION_ONLY_REQUIRED_FIELDS)
     return co
+
+
+@pytest.fixture(scope='module')
+def fake_courier_required_login_password():
+    co = CreateCourier()
+    login = fake.user_name()
+    password = fake.password()
+    co.courier_creation(data={'login': login, 'password': password})
+    return login, password
--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -16,13 +16,6 @@ class TestCourierLogin:
         lc.courier_login(data=SD.FAKE_COURIER_CREATION_ONLY_REQUIRED_FIELDS)
         lc.status_code_and_name(200, 'id')
 
-    @allure.title('Проверка ошибки при авторизации с неверным полем')
-    @pytest.mark.parametrize("login, password", [('Sasaraf', '342342'), ('Safaraz', '1234')])
-
-    def test_courier_auth_one_field_wrong(self, login, password):
-        lc = LoginCourier()
-        lc.courier_login(data={'login': login, 'password': password})
-        lc.check_status_code_and_text(404, 'message', 'Учетная запись не найдена')
 
     @allure.title('Проверка ошибки при авторизации с пустым полем')
     @pytest.mark.parametrize("login, password", [(fake.user_name(), ""), ("", fake.password())])
@@ -35,4 +28,18 @@ class TestCourierLogin:
     def test_non_existent_courier_login_failed(self):
         lc = LoginCourier()
         lc.courier_login(data={'login': fake.user_name(), 'password': fake.password})
+        lc.check_status_code_and_text(404, 'message', 'Учетная запись не найдена')
+
+    @allure.title('Проверка ошибки при авторизации с неверным полем логин')
+    def test_courier_auth_login_field_wrong(self, fake_courier_required_login_password):
+        login = fake_courier_required_login_password
+        lc = LoginCourier()
+        lc.courier_login(data={'login': login, 'password': '23432'})
+        lc.check_status_code_and_text(404, 'message', 'Учетная запись не найдена')
+
+    @allure.title('Проверка ошибки при авторизации с неверным полем пароль')
+    def test_courier_auth_password_field_wrong(self, fake_courier_required_login_password):
+        login = fake_courier_required_login_password
+        lc = LoginCourier()
+        lc.courier_login(data={'login': login, 'password': '23432'})
         lc.check_status_code_and_text(404, 'message', 'Учетная запись не найдена')
